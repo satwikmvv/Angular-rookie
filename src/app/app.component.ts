32962a07e1799ff292d50eddc5a9eaa9eecf8029
satwikmvv <satwikmvv@gmail.com>
2018-09-05 00:03:52 -0400

    minor tweaks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  ch = 'app';
+  Check(x) {
+    console.log(x)
+  }
 }
